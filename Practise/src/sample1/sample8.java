321fc614f0197692bd6cf773e2760a1f32bc0d46
prashanth.mudhelli <prashanth.mudhelli@servicenow.com>
2016-09-07 23:48:46 -0700

    Working on sorting

--- Practise/src/sample1/sample8.java
+++ Practise/src/sample1/sample8.java
@@ -4,8 +4,11 @@ public class sample8 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a[] = new int[10];
+		a[5]=1234;
+		int b[] = a;
+		b[5] = 5678;
+		System.out.println(a[5] +" " +b[5]);
 		
 	}
-
 }
-
